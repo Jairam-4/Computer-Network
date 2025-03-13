c7fad3ece0bfd944bf0b51626c0898f6a52fbde8
code with JaiRam <yedlajairam@gmail.com>
2025-03-13 14:07:36 +0530

    Create IP address yahoo .java

---
+++ IP address yahoo .java
@@ -0,0 +1,17 @@
+import java.net.*;
+import java.*;
+class ipaddr{
+    public static void main(String args[])
+    throws UnknownHostException
+    {
+        String s="https://www.yahoo.com/";
+        try{
+            InetAddress ip=InetAddress.getByName(new
+            URL(s).getHost());
+            System.out.println("public IP Address of:"+ip);
+        }
+        catch(MalformedURLException e){
+            System.out.println("invalid URL");
+        }
+    }
+}
