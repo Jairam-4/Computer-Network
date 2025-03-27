0cfcaad6d02be17a69fed1425bdce2bb7ea5016c
code with JaiRam <yedlajairam@gmail.com>
2025-03-27 13:14:14 +0530

    Update IP address yahoo .java

--- IP address yahoo .java
+++ IP address yahoo .java
@@ -1,16 +1,18 @@
 import java.net.*;
 import java.*;
-class ipaddr{
+class ipaddr
+{
     public static void main(String args[])
     throws UnknownHostException
     {
         String s="https://www.yahoo.com/";
-        try{
-            InetAddress ip=InetAddress.getByName(new
-            URL(s).getHost());
+        try
+        {
+            InetAddress ip=InetAddress.getByName(new URL(s).getHost());
             System.out.println("public IP Address of:"+ip);
         }
-        catch(MalformedURLException e){
+        catch(MalformedURLException e)
+        {
             System.out.println("invalid URL");
         }
     }
